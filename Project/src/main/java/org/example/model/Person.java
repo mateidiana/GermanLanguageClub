3927a67afdf9523a27c84a5fb1ecb113ba29930f
mateidiana <mateidiana10@gmail.com>
2024-11-06 08:46:36 +0200

    added in model

--- Project/src/main/java/org/example/model/Person.java
+++ Project/src/main/java/org/example/model/Person.java
@@ -1,4 +1,47 @@
 package org.example.model;
+import java.util.Objects;
 
 public class Person {
+    private int ID;
+    private String name;
+    private String surname;
+
+    public Person(int ID, String name, String surname){
+        this.ID=ID;
+        this.name=name;
+        this.surname=surname;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + ID +
+                ", username='" + name + '\'' +
+                ", password='" + surname + '\'' +
+                '}';
+    }
+
+    public int getId() {
+        return ID;
+    }
+
+    public void setId(int id) {
+        this.ID = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
 }
