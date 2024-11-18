93140d0d5a4e21f33df0b37185708a96c9e765d6
cowmaster99 <rockstarchica99@gmail.com>
2024-11-18 20:54:23 +0100

    review past mistakes mechanic for grammar, practice vocabulary and review past mistakes functions for vocabulary

--- Project/src/main/java/org/example/model/Vocabulary.java
+++ Project/src/main/java/org/example/model/Vocabulary.java
@@ -1,6 +1,9 @@
 package org.example.model;
+import java.util.Map;
+import java.util.HashMap;
 
 public class Vocabulary extends Course{
+    Map <String, String> worter=new HashMap<>();
     public Vocabulary(Integer id, String courseName, Teacher teacher, Integer maxStudents)
     {
         super(id, courseName, teacher, maxStudents);
@@ -12,5 +15,15 @@ public class Vocabulary extends Course{
                 "id=" + this.getId() +
                 ", name='" + this.getCourseName() + '\'' +
                 '}';
+    }
+    public Map<String, String> getWorter() {
+        return worter;
+    }
+
+    public void setWorter(Map<String, String> worter) {
+        this.worter = worter;
+    }
+    public void addWort(String key, String value) {
+        this.worter.put(key, value);
     }
 }
--- Project/src/main/java/org/example/service/VocabService.java
+++ Project/src/main/java/org/example/service/VocabService.java
@@ -1,7 +1,7 @@
 package org.example.service;
 import java.util.Scanner;
 import java.util.List;
-import java.util.Random;
+import java.util.*;
 
 import org.example.model.*;
 import org.example.model.Vocabulary;
@@ -30,18 +30,89 @@ public class VocabService {
             studentRepo.save(student);
         }
     }
+
     public void practiceVocabulary(Integer studentId, Integer courseId) {
         Student student = studentRepo.getById(studentId);
         Vocabulary course = vocabRepo.getById(courseId);
 
         Scanner scanner = new Scanner(System.in);
-
+        int correctAnswers=0;
         int foundCourse=0;
-        if (foundCourse==0)
-            System.out.println("\n\n\nYou are not enrolled in this course!");
+        Map <String, String> tempother=new HashMap<>();
+        for (Course findCourse : student.getCourses()){
+            if (findCourse.getId()==course.getId())
+            {
+                foundCourse=1;
+                break;}
+            else if (foundCourse==0){
+                System.out.println("\n\n\nYou are not enrolled in this course!");}}
+        if(foundCourse==1){
+            System.out.println("PLease write the correct translation for evey word (capital letter if needed):");
+            String placeholderKey=new String();
+            String placeholderValue=new String();
+            for(int i=1; i<10; i++){
+                List<String> values = new ArrayList<>(course.getWorter().values());
+                Random random = new Random();
+                int randomIndex = random.nextInt(values.size());
+                String ubung = values.get(randomIndex);
+                String answer=scanner.nextLine();
+                boolean found=false;
+                for (Map.Entry<String, String> entry : course.getWorter().entrySet()) {
+                    String key=entry.getKey();
+                    String value=entry.getValue();
+                    if(value==ubung && key==answer){
+                        System.out.println("Correct!");
+                        found=true;
+                    }
+                    else{
+                        placeholderKey=key;
+                        placeholderKey=value;
+                    }
+                }
+                if(found==true)
+                    correctAnswers++;
+                else{
+                    tempother=student.getPastVocabMistakes();
+                    tempother.put(placeholderKey, placeholderValue);
+                    student.setPastVocabMistakes(tempother);
+                }
+            }
+            if(correctAnswers>5) System.out.println("You have passed this practice test with the grade "+correctAnswers+"!");
+            else System.out.println("You have failed this practice test with the grade "+correctAnswers+". Do better, loser");
+        }
 
 
-
+    }
+    public void reviewPastMistakes(Integer studentId, Integer courseId) {
+        Student student = studentRepo.getById(studentId);
+        Vocabulary course = vocabRepo.getById(courseId);
+        Map <String, String> tempother=new HashMap<>();
+        Scanner scanner = new Scanner(System.in);
+        int foundCourse = 0;
+        for (Course findCourse : student.getCourses()) {
+            if (findCourse.getId() == course.getId()) {
+                foundCourse = 1;
+                break;
+            } else if (foundCourse == 0) {
+                System.out.println("\n\n\nYou are not enrolled in this course!");
+            }
+        }
+        if (foundCourse == 1) {
+            System.out.println("PLease fill in the gaps with the correct word(use capital letter when noun):");
+            String placeholderKey = new String();
+            String placeholderValue = new String();
+            for (Map.Entry<String, String> entry : course.getWorter().entrySet()) {
+                String key = entry.getKey();
+                String value = entry.getValue();
+                String answer = scanner.nextLine();
+                if (answer == key) {
+                    System.out.println("Correct!");
+                } else {
+                    tempother.put(key, value);
+                }
+            }
+            student.setPastVocabMistakes(tempother);
+        }
     }
 
     public List<Vocabulary> getAvailableCourses() {
