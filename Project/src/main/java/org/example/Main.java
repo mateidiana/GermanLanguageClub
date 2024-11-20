2f9ee100c54c34d5a17e637ec4a99b8421051773
mateidiana <mateidiana10@gmail.com>
2024-11-20 16:46:57 +0200

    added javadocs to repository

--- Project/src/main/java/org/example/Main.java
+++ Project/src/main/java/org/example/Main.java
@@ -329,23 +329,7 @@ public class Main {
         Exam exam2=new Exam(2,"GrammarExam1",new Teacher("Birigite",2));
         Exam exam3=new Exam(3,"VocabularyExam1",new Teacher("Monica",2));
         Exam exam4=new Exam(4,"WritingExam1",new Teacher("Cristian Matei",2));
-        String[][] exercises = {
 
-                {"Du brauchst Hilfe.", "Du _ Hilfe.", "a. brauchst", "b. braucht", "c. brauche", "You need help.", "a. brauchst"},
-                {"Eine rote Jacke.", "Eine _ Jacke.", "a. rote", "b. roten", "c. roter", "A red jacket.", "a. rote"},
-                {"Ich muss nach Berlin fahren.", "Ich muss _ Berlin fahren.", "a. in", "b. nach", "c. auf", "I have to go to Berlin.", "b. nach"},
-                {"Wir haben eine große Küche.", "Wir _ eine große Küche.", "a. haben", "b. habe", "c. hassen", "We have a big kitchen.", "a. haben"},
-                {"Ich brauche einen neuen Rucksack.", "Ich brauche einen _ Rucksack.", "a. nehmen", "b. neuen", "c. nachbaren", "I need a new backpack.", "b. neuen"},
-                {"Ich bin aus Frankfurt.", "Ich _ aus Frankfurt.", "a. bin", "b. bist", "c. bim", "I am from Frankfurt.", "a. bin"},
-                {"Hallo, ich bin Luca.", "Hallo, _ bin Luca.", "a. ich", "b. du", "c. er", "Hello, I am Luca.", "a. ich"},
-                {"Laura wohnt in Italien.", "Laura _ in Italien.", "a. wir", "b. wahnt", "c. wohnt", "Laura lives in Italy.", "c. wohnt"},
-                {"Sie sind von hier.", "Sie _ von hier.", "a. seien", "b. sind", "c. sinnen", "They are from here.", "b. sind"},
-                {"Du sprichst Italienisch.", "Du _ Italienisch", "a. sprichst", "b. sprechst", "c. sprich", "You speak Italian.", "a. sprichst"},
-                {"Magst du Englisch studieren?", "Magst du Englisch _ ?", "a. studieren", "b. studierst", "c. studierest", "Do you like to study English?", "a. studieren"},
-                {"Er arbeitet am Donnerstag.", "Er _ am Donnerstag.", "a. arbeitest", "b. arbeite", "c. arbeitet", "He works on Thursday.", "c. arbeitet"},
-                {"Du hast einen Hund, nicht?", "Du hast _ Hund, nicht?", "a. ein", "b. einen", "c. einer", "You have a dog, right?", "b. einen"},
-                {"Ich mag diese Wohnung.", "Ich mag _ Wohnung.", "a. dieses", "b. dieser", "c. diese", "I like this living space.", "c. diese"}
-        };
         String [][] grammarExercises={
                 { "Du (brauchen) _ Hilfe.", "brauchst" },
                 { "Ich bin _ Hause.", "zu" },
--- Project/src/main/java/org/example/repo/VocabRepository.java
+++ Project/src/main/java/org/example/repo/VocabRepository.java
@@ -4,23 +4,62 @@ import org.example.model.Vocabulary;
 
 import java.util.ArrayList;
 import java.util.List;
+
+/**
+ * A singleton repository class for managing {@link Vocabulary} objects.
+ * Implements the {@link IRepository} interface to provide CRUD operations.
+ * This class ensures that there is only one instance of the repository
+ * throughout the application lifecycle.
+ */
 public class VocabRepository implements IRepository<Vocabulary> {
+
+    /**
+     * A list of {@link Vocabulary} objects managed by the repository.
+     * This list holds all vocabulary courses in memory.
+     */
     private List<Vocabulary> vocabCourses;
+
+    /**
+     * A static instance of the {@link VocabRepository} for the singleton pattern.
+     * Ensures only one instance of the repository exists.
+     */
     private static VocabRepository instance;
+
+    /**
+     * Constructs a new {@link VocabRepository} with an empty list of vocabulary courses.
+     * This constructor is private to ensure the singleton pattern.
+     */
     public VocabRepository() {
         this.vocabCourses=new ArrayList<>();
     }
 
+    /**
+     * Retrieves all {@link Vocabulary} objects managed by the repository.
+     *
+     * @return A list of all {@link Vocabulary} objects in the repository.
+     */
     @Override
     public List<Vocabulary> getObjects(){
         return vocabCourses;
     }
 
+    /**
+     * Saves a new {@link Vocabulary} object to the repository.
+     *
+     * @param entity The {@link Vocabulary} object to be saved.
+     */
     @Override
     public void save(Vocabulary entity) {
         vocabCourses.add(entity);
     }
 
+    /**
+     * Updates an existing {@link Vocabulary} object in the repository.
+     * The method will replace the given {@link Vocabulary} object with a new one.
+     *
+     * @param entity The {@link Vocabulary} object to be updated.
+     * @param VocabularyRepl The new {@link Vocabulary} object to replace the old one.
+     */
     @Override
     public void update(Vocabulary entity, Vocabulary VocabularyRepl) {
         int index = vocabCourses.indexOf(entity);
@@ -29,6 +68,11 @@ public class VocabRepository implements IRepository<Vocabulary> {
         }
     }
 
+    /**
+     * Deletes a {@link Vocabulary} object from the repository.
+     *
+     * @param object The {@link Vocabulary} object to be deleted.
+     */
     @Override
     public void delete(Vocabulary object) {
         vocabCourses.remove(object);
